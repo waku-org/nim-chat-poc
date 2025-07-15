e328d827dbb7df09e6d1b1e11f64486735586dc5
Jazz Turner-Baggs <473256+jazzz@users.noreply.github.com>
2025-07-14 19:39:21 -0700

    Add ECDH functions

---
+++ src/crypto/ecdh.nim
@@ -0,0 +1,43 @@
+import results
+import libp2p/crypto/curve25519
+import bearssl/rand
+
+type PrivateKey* = object
+  bytes: Curve25519Key
+
+type PublicKey* = object
+  bytes: Curve25519Key
+
+
+proc bytes*(key: PrivateKey): Curve25519Key =
+  return key.bytes
+
+proc bytes*(key: PublicKey): Curve25519Key =
+  return key.bytes
+
+proc createRandomKey*(): Result[PrivateKey, string] =
+  let rng = HmacDrbgContext.new()
+  if rng.isNil:
+    return err("Failed to create HmacDrbgContext with system randomness")
+  ok(PrivateKey(bytes: Curve25519Key.random(rng[])))
+
+proc loadKeyFromBytes*(bytes: openArray[byte]): Result[PrivateKey, string] =
+  if bytes.len != Curve25519KeySize:
+    return err("Private key size must be 32 bytes")
+  ok(PrivateKey(bytes: intoCurve25519Key(bytes)))
+
+
+proc getPublicKey*(privateKey: PrivateKey): PublicKey =
+  PublicKey(bytes: public(privateKey.bytes))
+
+
+proc Dh*(privateKey: PrivateKey, publicKey: PublicKey): Result[seq[
+    byte], string] =
+
+  var outputKey = publicKey.bytes
+  try:
+    Curve25519.mul(outputKey, privateKey.bytes)
+  except CatchableError as e:
+    return err("Failed to compute shared secret: " & e.msg)
+
+  return ok(outputKey.getBytes())
