d1189ea3057c142859cf2524d90726ab28060335
Jazz Turner-Baggs <473256+jazzz@users.noreply.github.com>
2025-09-26 15:04:09 -0700

    update more exports

--- src/chat_sdk.nim
+++ src/chat_sdk.nim
@@ -2,7 +2,13 @@ import chat_sdk/[
   client,
   conversations,
   delivery/waku_client,
-  links
+  identity,
+  links,
+  proto_types
+
 ]
 
-export client, conversations, waku_client, links
+export client, conversations, waku_client, identity, links
+
+#export specific frames need by applications
+export ContentFrame
