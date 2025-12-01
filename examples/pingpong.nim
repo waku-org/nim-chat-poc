4700223b843c6189943eb0e4c1d54a29b26598ed
Jazz Turner-Baggs <473256+jazzz@users.noreply.github.com>
2025-11-30 20:38:55 -0800

    Rename module from chat_sdk to chat (#24)

--- examples/pingpong.nim
+++ examples/pingpong.nim
@@ -2,11 +2,11 @@ import chronicles
 import chronos
 import strformat
 
-import chat_sdk
+import chat
 import content_types
 
 # TEsting
-import ../src/chat_sdk/crypto
+import ../src/chat/crypto
 
 
 proc getContent(content: ContentFrame): string =
